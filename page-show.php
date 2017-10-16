8827f638ad69938e7220fc17ad40c56e5d84d797
Pavel Elfimov <pelfimov@ya.ru>
2017-10-16 12:59:56 +0700

    Делаю страницы шоу

--- page-show.php
+++ page-show.php
@@ -6,10 +6,10 @@ Template Name: Страницы ШОУ
 <?php get_header(); ?>
 	<main class="show-description">
 		<div class="show-description__wrapper">
-			<a class="btn-close show-description__btn-close" href="index.html">Закрыть</a>
+			<a class="btn-close show-description__btn-close" href="<?php echo home_url();?>">Закрыть</a>
 			<article class="show-description__article">
 				<section>
-					<?php if (have_posts()): while (have_posts()): the_post(); ?>
+						<?php if (have_posts()): while (have_posts()): the_post(); ?>
 						<?php the_content(); ?>
 					<?php endwhile; endif; ?>
 				</section>
